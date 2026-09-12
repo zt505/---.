5d0bc22b1044ac7faa748bb2ed284db63f5945a0
zt505 <mshq115@gmail.com>
2026-09-13 00:47:43 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>أهلاً بك في موقعي!</h1>
+      <p>تم إعداد الصفحة بنجاح داخل مجلد app</p>
+    </div>
+  );
+}
